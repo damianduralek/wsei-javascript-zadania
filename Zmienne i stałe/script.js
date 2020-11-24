5491cbf2a7b0107724e25da4572df3444a479125
damianduralek <72334876+damianduralek@users.noreply.github.com>
2020-11-24 15:19:55 +0100

    Update script.js

---
+++ Zmienne i stałe/script.js
@@ -0,0 +1,12 @@
+Zadanie 1
+
+var number = 3;
+console.log(number); //wypisuje: 3
+var string = "słowo";
+console.log(string);  //wypisuje: słowo
+var number_string = 2 + "text";
+console.log(number_string); //wypisuje: 2text
+var logic = true;
+console.log(logic);  //wypisuje wartość logiczną: true
+var special = null;
+console.log(special);  //wypisuje: null
