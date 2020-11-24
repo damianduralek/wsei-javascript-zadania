d7bd828db15fc1133db1cdef969116cc81d0a081
damianduralek <72334876+damianduralek@users.noreply.github.com>
2020-11-24 13:52:35 +0100

    Update script.js

--- Początki/script.js
+++ Początki/script.js
@@ -1,5 +1,15 @@
 Zadanie 1
 
-console.log("Twoje imię");
+console.log("Twoje imię"); 
 
 Zadanie 2
+
+console.log(12 + 32); // wynik 44
+console.log(52 / 16); // wynik 3,25
+console.log(5 * 2); // wynik 10
+
+Zadanie 3
+
+console.log("Hello World"); // wypisze Hello World
+console.log(Hello World);  // błąd
+console.log("Hello World); // błąd
